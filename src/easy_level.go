95b5c779cb999616cfb19fe041a70b528e9e1b46
kami <zh_guomail@163.com>
2021-06-08 22:33:08 +0800

    [算法]897. Increasing Order Search Tree递归

--- src/easy_level.go
+++ src/easy_level.go
@@ -10,6 +10,27 @@ func sumBase(n int, k int) (sum int) {
 	return
 }
 
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//897. Increasing Order Search Tree递归
+func increasingBST(root *TreeNode) *TreeNode {
+	return recurveLeft(root, nil)
+}
+func recurveLeft(root *TreeNode, tail *TreeNode) *TreeNode {
+	if root == nil {
+		return tail
+	}
+	var res = recurveLeft(root.Left, root)
+	root.Left = nil
+	root.Right = recurveLeft(root.Right, tail)
+	return res
+}
+
 func main() {
 	res := sumBase(34, 6)
 	fmt.Println(res)
