a3e82861d22fa44fcf80b5a7e1d65d4a1f760dac
kami <zh_guomail@163.com>
2021-07-11 22:37:57 +0800

    127. Word Ladder未完成

---
+++ src/leetcode/top/top_interview.go
@@ -0,0 +1,20 @@
+package top
+
+/**
+ * @description: 127. Word Ladder
+	A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words
+	beginWord -> s1 -> s2 -> ... -> sk such that:
+
+	Every adjacent pair of words differs by a single letter.
+	Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+	sk == endWord
+	Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest
+	transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+ * @return: 最短路径长度
+ * @author: kami
+ * @关键词：TODO
+ * @date: 2021/7/11 21:55
+*/
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+	return 0
+}
